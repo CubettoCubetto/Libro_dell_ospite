337ecffe37a347ec4742524245f62abb7727de33
Diego Pirovano <139859822+CubettoCubetto@users.noreply.github.com>
2024-06-26 19:56:15 +0200

    Create a simple index with the url to access faster and u

--- home/index.js
+++ home/index.js
@@ -1,6 +1,8 @@
-var global_choice = false
 let urlServer = "https://diegopirovano.pythonanywhere.com"
 //urlServer = "http://127.0.0.1:8013"
+
+var global_choice = false
+
 //caricare il username della pagina
 const urlParams = new URLSearchParams(window.location.search);
 const username = urlParams.get('username')
