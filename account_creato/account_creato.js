59bb54a9fc13e137405935a81b240d491e7cd65e
Diego Pirovano <139859822+CubettoCubetto@users.noreply.github.com>
2024-06-29 15:45:45 +0200

    Created QRcode functions

--- account_creato/account_creato.js
+++ account_creato/account_creato.js
@@ -7,5 +7,23 @@ var global_choice = false
 const urlParams = new URLSearchParams(window.location.search);
 const username = urlParams.get('username')
 
+url_to_send_message = "https://diegopirovano.pythonanywhere.com/sh/"+username
+
 // cambiare il nome del titolo
-document.getElementById("titolo").innerHTML="Bentornato " + username + "!"
+document.getElementById("titolo").innerHTML="Ciao " + username + ", abbiamo aggiornato il tuo account!"
+document.getElementById("url-invia-commento").innerHTML= '<a target="_blank" href='+url_to_send_message+'> Puoi inviare un commento a questo link </a>'
+
+const qrcode = new QRCode(document.getElementById("qrcode"), {
+    text: url_to_send_message,
+    width: 256,
+    height: 256
+});
+
+document.getElementById('downloadQR').addEventListener('click', function() {
+    const qrCanvas = document.querySelector('#qrcode canvas');
+    const qrDataUrl = qrCanvas.toDataURL('image/png');
+    const link = document.createElement('a');
+    link.href = qrDataUrl;
+    link.download = 'qrcode.png';
+    link.click();
+});
