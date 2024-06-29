26ae879ebdbebd9a60d264fc2c6013035643dcf1
Diego Pirovano <139859822+CubettoCubetto@users.noreply.github.com>
2024-06-29 17:36:36 +0200

    modified data order: password is now in third postition

--- crea_account/crea_account.js
+++ crea_account/crea_account.js
@@ -13,6 +13,7 @@ document.getElementById('createAccountForm').addEventListener('submit', function
     const formData = new FormData();
     formData.append('formFile', document.getElementById('formFile').files[0]);
     formData.append('text1', document.getElementById('text1').value);
+    formData.append('password', document.getElementById('password').value);
     formData.append('text3', document.getElementById('text3').value);
     formData.append('star1', document.getElementById('star1').value);
     formData.append('star2', document.getElementById('star2').value);
@@ -20,7 +21,6 @@ document.getElementById('createAccountForm').addEventListener('submit', function
     formData.append('star4', document.getElementById('star4').value);
     formData.append('star5', document.getElementById('star5').value);
     formData.append('name', document.getElementById('name').value);
-    formData.append('password', document.getElementById('password').value);
     formData.append('providedPassword', document.getElementById('providedPassword').value);
 
     for (const value of formData.values()) {
