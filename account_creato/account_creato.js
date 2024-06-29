d622b3e77fc61329042eeb364dbe56c2cac8dfa2
Diego Pirovano <139859822+CubettoCubetto@users.noreply.github.com>
2024-06-29 17:06:34 +0200

    Update my logo with user logo in account_created

--- account_creato/account_creato.js
+++ account_creato/account_creato.js
@@ -9,6 +9,21 @@ const username = urlParams.get('username')
 
 url_to_send_message = "https://diegopirovano.pythonanywhere.com/sh/"+username
 
+// aggiungere il logo alla pagina
+// Create a new image element 
+var img = document.createElement('img'); 
+ 
+// Set the source (src) attribute of the image 
+img.src = urlServer+"/get_image/"+username; 
+
+// Set any additional attributes like alt text, width, height, etc. 
+img.alt = 'Logo';  
+
+// Get a reference to the container where you want to insert the image 
+var container = document.getElementById('imageLogoContainer'); 
+
+// Append the image element to th
+
 // cambiare il nome del titolo
 document.getElementById("titolo").innerHTML="Ciao " + username + ", abbiamo aggiornato il tuo account!"
 document.getElementById("url-invia-commento").innerHTML= '<a target="_blank" href='+url_to_send_message+'> Puoi inviare un commento a questo link </a>'
