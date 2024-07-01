61f5f73f4d9535dbd83e4bea1a45b8a82918322a
Diego Pirovano <139859822+CubettoCubetto@users.noreply.github.com>
2024-07-01 21:47:04 +0200

    fixed a bug

--- crea_account/crea_account.js
+++ crea_account/crea_account.js
@@ -20,7 +20,7 @@ document.getElementById('createAccountForm').addEventListener('submit', function
 
     const formData = new FormData();
     formData.append('formFile', document.getElementById('formFile').files[0]);
-    formData.append('text2', document.getElementById('text1').value.trim());
+    formData.append('text2', document.getElementById('text2').value.trim());
     formData.append('password', document.getElementById('password').value.trim());
     formData.append('username', document.getElementById('text1').value);
     formData.append('text3', document.getElementById('text3').value);
